4a34de454bb18b039a12e26e2a5198920fa6dd41
Biagio Vincenzo Mamone <contato@bmamone.com>
2025-11-18 07:18:33 -0300

    added i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+    /* config options here */
+    reactStrictMode: true,
+    i18n: {
+        locales: ['en-US', 'pt-BR'],
+        defaultLocale: 'en-US',
+    },
 };
 
 export default nextConfig;
